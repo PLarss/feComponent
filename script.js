f04ee37a7d247f8cc1eea0c6a9910c0201ef49f4
PLarss <per.larsson@hm.com>
2021-04-20 12:20:14 +0200

    Worked on Add(item) for basket

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // TODO: Add all twelve images to candies
 let candies = ["./images/1.jpeg", "./images/2.jpeg", "./images/3.jpeg", "./images/4.jpeg", "./images/5.jpeg" ];
 let candiesShow;
-let basket; 
+let b = []; 
 let basketstart = '<div class="grid-container" id="shoppingbag">'; 
 let basketend = '</div>'
 
@@ -11,28 +11,22 @@ initBasket();
 
 // Create code
 
-// TODO: Create a basket function
+// TODO: Create a basket function and stuff it to a basket 
 function add(item){
-    basket = basket + item;
+    let basket = ""; 
     console.log(basket);  
+    b.push(item)
+
+    for (let index = 0; index < b.length; index++) {
+        basket = basket + b[index] + '<br>';        
+    }
+
+    // TODO: WORK ON BASKET HTML! 
     document.getElementById("shoppingbag").innerHTML = basketstart + basket + basketend;     
+
+    // TODO: ADD CHECKOUT BUTTON in basket and in Menu
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
