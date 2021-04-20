ffb4f7dc3a45cef5dd348464dedaac8ff2d2d370
PLarss <per.larsson@hm.com>
2021-04-20 11:57:09 +0200

    reworked html and basket init

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // global variables
 // TODO: Add all twelve images to candies
-let candies = ["./images/1.jpeg", "./images/2.jpeg", "./images/3.jpeg", "./images/4.jpeg" ];
+let candies = ["./images/1.jpeg", "./images/2.jpeg", "./images/3.jpeg", "./images/4.jpeg", "./images/5.jpeg" ];
 let candiesShow;
+let basket; 
 
 initContent(); 
+initBasket();
 
 // Create code
 
-
-
-
-
+// TODO: Create a basket function
+function add(item){
+    basket = basket + item;
+    console.log(basket);    
+}
 
 
 
@@ -33,7 +36,14 @@ initContent();
 
 
 // Have a function to load all content // TODO: remove logging when getting further
-function initContent(){ candiesShow = '<div id="content">'; loadContent(); candiesShow = candiesShow + '</div>'; console.log(candiesShow); document.getElementById("content").innerHTML = candiesShow;
+function initContent(){ candiesShow = '<div class="grid-container" id="content">'; loadContent(); candiesShow = candiesShow + '</div>'; console.log(candiesShow); document.getElementById("content").innerHTML = candiesShow;
+}
+
+function initBasket(){
+    basket = '<div class="grid-container" id="shoppingbag">No items in the basket</div>';
+    document.getElementById("shoppingbag").innerHTML = basket;                    
 }
 // Supporting function to create HTML for InitContent()
-function loadContent(){ for (const candy of candies) { candiesShow = candiesShow + '<p><img src="' + candy + '" alt="Lägg till en text dynamiskt"></p>'; } }
+function loadContent(){ for (const candy of candies) { candiesShow = candiesShow + 
+    '<div class="grid-item"><img  onclick="add(\'' + candy + '\')" src="' + candy +'" alt="' + candy + '"></div>'
+    ; } }
