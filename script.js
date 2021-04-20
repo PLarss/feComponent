bf5f9373d5ab9b5d9e5a8dbfcd8f2e5110a03452
PLarss <per.larsson@hm.com>
2021-04-20 13:16:25 +0200

    worked on basket

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // global variables
 // TODO: Add all twelve images to candies
-let candies = ["./images/1.jpeg", "./images/2.jpeg", "./images/3.jpeg", "./images/4.jpeg", "./images/5.jpeg" ];
+let candies = ["./images/1.jpeg", "./images/2.jpeg", "./images/3.jpeg", "./images/4.jpeg", "./images/5.jpeg", "./images/6.jpeg", "./images/7.jpeg" ];
 let candiesShow;
 let b = []; 
 let basketstart = '<div class="grid-container" id="shoppingbag">'; 
@@ -13,23 +13,37 @@ initBasket();
 
 // TODO: Create a basket function and stuff it to a basket 
 function add(item){
-    let basket = ""; 
-    console.log(basket);  
-    b.push(item)
+    console.log("add function");
+    let basket = "";
+    let itemObject = ""; 
+    let itemExists = false;
+    // Kolla om item redan finns i b objektet, om så kolla index samt öka upp weight med 100. 
+    for (let index = 0; index < b.length; index++) {
+        if(item === b[index].name){
+            itemObject = {name: b[index].name, weight: b[index].weight + 100, price:b[index].price};
+            b[index] = itemObject;
+            itemExists = true
+            console.log("matched an item")
+        }
+    }
+    
+    if(itemObject == false){
+        console.log("add item");
+        let itemObject = {name: item, weight: 100, price:9.95};  
+        b.push(itemObject)
+    }
 
     for (let index = 0; index < b.length; index++) {
-        basket = basket + b[index] + '<br>';        
+        basket = basket + '<div class="itemInBasket"><img src="'+ b[index].name + '"><br>' + b[index].weight +' gram ' + '</div><br>';        
     }
 
     // TODO: WORK ON BASKET HTML! 
+    console.log("basket: " + basket);
     document.getElementById("shoppingbag").innerHTML = basketstart + basket + basketend;     
 
     // TODO: ADD CHECKOUT BUTTON in basket and in Menu
 
 }
-
-
-
 
 
 // Have a function to load all content // TODO: remove logging when getting further
